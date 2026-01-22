246dcd8d10d0e8b5317f53f7bc1e8e289839b5e0
Abhishekak71 <abhishekkumarms1621@gmail.com>
2026-01-23 01:02:31 +0530

    add some commit

--- week 3/Week-3.2/Database/app.js
+++ week 3/Week-3.2/Database/app.js
@@ -2,9 +2,9 @@ const express=require('express');
 const mongoose=require('mongoose');
 const port=3000;
 
-mongoose.connect("mongodb+srv://abhishek_ak71:MSdhoni7Srk@cluster0.7tkfdmr.mongodb.net/newuser")
+mongoose.connect("mongodb+srv://abhishek_ak71:MSdhoni7Srk@cluster0.7tkfdmr.mongodb.net/newuser")//connect to database
 
-const User=mongoose.model('user',{
+const User=mongoose.model('user',{//define a model, in which format your data will store 
     username: String,
      name: String, 
      password: String
@@ -18,18 +18,18 @@ app.post('/signup',async function(req,res){
     const password=req.body.password;
     const name=req.body.name;
 
-    const UserExist= await User.findOne({username: username});
+    const UserExist= await User.findOne({username: username}); // check is user already exist in database?
     if(UserExist){
         return res.status(400).json({
             msg: "user already there"
         });
     }
-    const user= new User({
+    const user= new User({  //create user name table
         username: username,
         password: password,
         name: name
     });
-    user.save();
+    user.save();        //save data to database
     return res.json({
         msg: "user save successfuly"
     })
